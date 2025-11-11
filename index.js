de36a205d7400abe9033befc365d5af8fd2ae63e
Imtiazul Hoque <imtiazulhoque8936@gmail.com>
2025-11-11 11:33:09 +0600

    mongodb successfully connected with the server

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongoDB Connetion
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dbz4f6f.mongodb.net/?appName=Cluster0`;
+// const uri = `${process.env.DB_LOCAL}`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("social DB is running on port:", port);
