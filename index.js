b92ecf24a08328cc0b9d875dd03b7d81fcb8a755
Imtiazul Hoque <imtiazulhoque8936@gmail.com>
2025-11-13 00:41:47 +0600

    get operation for showing defaultValue in the updateForm added

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/update-event/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await socialEventsCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
